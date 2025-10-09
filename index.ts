35e98cb21008d0d431482d2ca69c442ca5691ef3
Yana <y.minkova1@gmail.com>
2025-10-09 22:26:00 +0300

    typescript intro

---
+++ index.ts
@@ -0,0 +1,13 @@
+console.log("Hello! My first TypeScript code!")
+
+let fName: string = '';
+const age: number = 20;
+
+let isTrue = 5 > 7;
+console.log(isTrue)
+
+let singer: null = null;
+
+let lName: any = 100;
+
+let number: unknown = [50, 40];
